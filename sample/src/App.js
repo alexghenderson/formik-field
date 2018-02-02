739c40bd9dce756ff37af3d619fb841b331efc2d
Alex Henderson <alex@alexhenderson.me>
2018-02-01 22:50:23 -0800

    Updated sample

--- sample/src/App.js
+++ sample/src/App.js
@@ -10,16 +10,42 @@ class App extends Component {
         <Formik
           initialValues={{
             name: '',
+            email: '',
           }}
           validationSchema={yup.object().shape({
             name: yup.string().required().min(1).label('Name'),
+            email: yup.string().email().required().label('Email'),
           })}
-          onSubmit={(values)=>{window.alert(`Hello ${values.name}`)}}
+          onSubmit={(values)=>{window.alert(`Hello ${values.name} (${values.email})`)}}
           render={({handleSubmit})=>(
             <form onSubmit={handleSubmit}>
               <Field
                 name='name'
                 label='Name'
+                render={({handleChange, handleBlur, value, error, label})=>(
+                  <div>
+                    <div>{label}</div>
+                    <div>
+                      <input
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={value}
+                      />
+                    </div>
+                    {
+                      error &&
+                      <div>
+                        {error}
+                      </div>
+                    }
+                  </div>
+                )}
+              />
+              <Field
+                name='email'
+                label='Email (normalized to lower case, formatted to upper case)'
+                normalize={(val='')=>(val.toLowerCase())}
+                format={(val='')=>(val.toUpperCase())}
                 render={({handleChange, handleBlur, value, error, label})=>(
                   <div>
                     <div>{label}</div>
