9f6f5a7fd6f7b7ffb351f3df737acf227ac64c69
Alex Henderson <alex@alexhenderson.me>
2018-02-01 22:32:51 -0800

    Revise sample

--- sample/src/App.js
+++ sample/src/App.js
@@ -1,37 +1,3 @@
-## Overview
-I started using Formik recently, and so far, I really like it. But I wanted to use render props for my field, and I wanted to simplify writing my fields. So I wrote this.
-
-## Installation
-Install formik:
-```bash
-npm install --save formik
-```
-
-Install formik field:
-```bash
-npm install --save formik-field
-```
-
-## Usage
-Formik field provided a `Field` component which uses context provided by formik forms. As such, it must be nested inside a formik form.
-
-Formik field takes a few props:
-* `name: string` name of the field
-* `label: string` label for the field
-* `normalize?: (val: any)=>(any)` function for normalizing values prior to being stored in state
-* `format?: (val: any)=>(any)` function for formatting values before being passed to the field
-
-and one of the two following render properties
-* `render` or `children`
-
-The render function has the following signature:
-* `render({label, name, handleChange, handleBlur, value, error, touched})=>ReactNode`
-
-I wrote this to automate the mapping of the event handlers, touched, error and value objects.
-
-## Example
-
-```js
 import React, { Component } from 'react';
 import {Formik} from 'formik';
 import Field from 'formik-field';
@@ -81,7 +47,5 @@ class App extends Component {
     );
   }
 }
-```
 
-## Tips
-Make a snippet for this. 
+export default App;
